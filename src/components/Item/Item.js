982afbb9488154fab2801f10ec71b47019f1a37d
khaledwani27 <wanikhaled27@gmail.com>
2022-06-28 17:04:04 +0300

    itrm biding room built without any css

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -3,19 +3,17 @@ import React from "react";
 function Item(props) {
   return (
     <div>
-      <div>{props.itemTitle}</div>
-      <img src={props.itemImg} />
-      <div>{props.itemPrice}</div>
-      <div>{props.itemTimer}</div>
-      <div>{props.itemDescription}</div>
+      <div>{props.item.itemTitle}</div>
+      <img src={props.item.itemImg} alt=''/>
+      <div>Price: {props.item.itemPrice}</div>
+      <div>Remaining time: {props.item.itemTimer}</div>
     </div>
   );
 }
 
 // <Item
 //         itemTitle="iphone 13"
-//         itemPrice={5}
-//         itemDescription="phone as new, without scratches"
+//         itemPrice={5}  
 //         itemTimer="2:01:00"
 //         itemImg="https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-13-01.jpg"
 //       />
