79b39740985f02bbe5523094735a883c7858c94b
ahmadgabariin <ahmadhamze.kawasaki@gmail.com>
2022-06-28 19:02:32 +0300

    Searchbar and some stuff

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Searchbar.css'
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 function Searchbar() {
   return (
     <div className='search-bar'>
-      <input placeholder=''></input>  
-      <button>Search</button>
+      <TextField id="standard-basic" label="Search" variant="standard" />
+      <Button variant="contained">Hello World</Button>
     </div>
   )
 }
