9c919c806b4b6a38303a3a5496a0b0af8a4a8a7b
loai abed alhi <loai007thbk@hotmail.com>
2022-06-29 11:39:22 +0300

    Item component remover from itemRoom

--- src/components/ItemRoom/ItemRoom.js
+++ src/components/ItemRoom/ItemRoom.js
@@ -1,40 +1,52 @@
-import React from 'react'
-import Item from '../Item/Item'
-import data from '../../data.json'
-import './ItemRoom.css'
-import Button from '@mui/material/Button';
-import AlarmIcon from '@mui/icons-material/Alarm';
-import TextField from '@mui/material/TextField';
+import React from "react";
+import Item from "../Item/Item";
+import data from "../../data.json";
+import "./ItemRoom.css";
+import Button from "@mui/material/Button";
+import AlarmIcon from "@mui/icons-material/Alarm";
+import TextField from "@mui/material/TextField";
 function ItemRoom() {
-
-  function inputHandler(){
-
-  }
+  function inputHandler() {}
 
   return (
-    <div className='room'>
-
-      <div className='item-details'>
-        <Item item={data[0]}/>
-        <p>{data[0].itemDescription}</p> 
-        <div className='btn-input-bid-container'>
-          <TextField id="outlined-basic" label="Bid" variant="outlined" size='small' 
-           type="number" name="bidingInput"  onChange={inputHandler} className = "bid-input" />
-          <Button variant="contained" color="success"  className='btn-bid' size='small'>
-            <AlarmIcon/>Bid
+    <div className="room">
+      <div className="item-details">
+        <div className="item-title">{data[0].itemTitle}</div>
+        <div>Time Left : {data[0].itemTimer}</div>
+        <img src={data[0].itemImg} alt="" />
+        <p>{data[0].itemDescription}</p>
+        <div>Current bid : {data[0].itemPrice}</div>
+        <div className="btn-input-bid-container">
+          <TextField
+            id="outlined-basic"
+            label="Bid"
+            variant="outlined"
+            size="small"
+            type="number"
+            name="bidingInput"
+            onChange={inputHandler}
+            className="bid-input"
+          />
+          <Button
+            variant="contained"
+            color="success"
+            className="btn-bid"
+            size="small"
+          >
+            <AlarmIcon />
+            Bid
           </Button>
         </div>
       </div>
 
-      <div className='side-bar-padding-history'>
-        <div className='bidder-history' >
+      <div className="side-bar-padding-history">
+        <div className="bidder-history">
           <span>khaledwani: </span>
           <span>$10</span>
         </div>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default ItemRoom
+export default ItemRoom;
