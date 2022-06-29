ed0cdeae6a1e1f3599d5bebf0641d8c3484df7c8
khaledwani27 <wanikhaled27@gmail.com>
2022-06-29 23:21:52 +0300

    add the permissions to the server

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ const mongoose = require(`mongoose`)
 
 mongoose.connect(`mongodb://localhost/Auction`, { useNewUrlParser: true })
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+    next()
+})
+
 app.use(express.json())
 app.use( express.urlencoded( { extended : false } ) )
 app.use(`/`, api)
