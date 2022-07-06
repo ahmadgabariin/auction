409cb60e43cbc29ed01a0cf4fc9514382cde7761
loai abed alhi <loai007thbk@hotmail.com>
2022-07-06 18:31:12 +0300

    items routs filter by category

--- server/server/routes/itemAPI.js
+++ server/server/routes/itemAPI.js
@@ -1,59 +1,84 @@
-const express = require(`express`)
-const router = express.Router()
-const User = require(`../models/User`)
-const Item = require(`../models/Item`)
+const express = require(`express`);
+const router = express.Router();
+const User = require(`../models/User`);
+const Item = require(`../models/Item`);
+const data = require("../../data.json");
 
 router.get(`/items`, function (request, response) {
-    response.send(`get items`)
-})
+  let category = request.query.category;
+  if (category) {
+    Item.find(
+      {
+        category: category,
+      },
+      async function (err, items) {
+        if (err) {
+          response.status(500).send(err);
+          return;
+        } else if (!items) {
+          response.status(404).send("Item Not Found");
+          return;
+        } else if (items) {
+          response.status(200).send(items);
+          return;
+        }
+        response.status(200).send(items);
+        return;
+      }
+    );
+  } else {
+    Item.find({}, function (err, items) {
+      response.status(200).send(items);
+    });
+  }
+});
 
 router.delete(`/item`, function (request, response) {
-    response.send(`delete item`)
-})
+  response.send(`delete item`);
+});
 router.put(`/item`, function (request, response) {
-    response.send(`update item`)
-})
+  response.send(`update item`);
+});
 
 router.post(`/item`, function (request, response) {
-    const itemData = request.body
-    const item = new Item({
-        title :itemData.itemTitle,
-        category :itemData.category,
-        image :itemData.itemImg, //firebase link
-        price : itemData.itemPrice,
-        isApproved :false,
-        description :itemData.itemDescription,
-        
+  const itemData = request.body;
+  const item = new Item({
+    title: itemData.itemTitle,
+    category: itemData.category,
+    image: itemData.itemImg, //firebase link
+    price: itemData.itemPrice,
+    isApproved: false,
+    description: itemData.itemDescription,
+  });
+  item
+    .save()
+    .then((i) => {
+      response.status(201).send({ message: "item was added", data: i });
     })
-    item.save().then(i => {
-        response.status(201).send({ message: "item was added", data: i })
-    }).catch(err => {
-        response.status(400).send({ message: "can not add new item", err: err })
-    })
-    
+    .catch((err) => {
+      response.status(400).send({ message: "can not add new item", err: err });
+    });
+});
 
-})
+module.exports = router;
 
-module.exports = router
+// const us1 = new User({
+//     username : `khaled` ,
+//     password : `awdawd`
+// })
 
+// us1.save()
 
-    // const us1 = new User({
-    //     username : `khaled` ,
-    //     password : `awdawd`
-    // })
+// response.send(`adwawd`)
 
-    // us1.save()
+// // User.findById(`62bca28ac15eb10443c16262` , function (error , data ){
+// //     const item1 = new Item({
+// //         title : "Iphone XS" ,
+// //         category : "phones"
+// //     })
 
-    // response.send(`adwawd`)
-
-    // // User.findById(`62bca28ac15eb10443c16262` , function (error , data ){
-    // //     const item1 = new Item({
-    // //         title : "Iphone XS" ,
-    // //         category : "phones"
-    // //     })
-
-    // //     item1.save()
-    // //     data.Items.push(item1)
-    // //     data.save()
-    // //     response.send(data)
-    // // })
+// //     item1.save()
+// //     data.Items.push(item1)
+// //     data.save()
+// //     response.send(data)
+// // })
