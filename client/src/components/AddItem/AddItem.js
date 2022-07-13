814099af3d5a905826ca3760c3f3c35d11c0dfed
loai abed alhi <loai007thbk@hotmail.com>
2022-07-13 21:19:22 +0300

    fixed some conflicts

--- client/src/components/AddItem/AddItem.js
+++ client/src/components/AddItem/AddItem.js
@@ -17,19 +17,11 @@ function AddItem() {
     price: 0,
   });
   const handleChanges = (e) => {
-<<<<<<< HEAD
-    setInputsValues({ ...inputsValues, [e.target.name]: e.target.value });
+    if (e.target.value != -1) {
+      setInputsValues({ ...inputsValues, [e.target.name]: e.target.value });
+    }
   };
 
-=======
-    if (e.target.value != -1)
-    {
-      setInputsValues({ ...inputsValues, [e.target.name]: e.target.value })
-    }
-    
-  }
-  
->>>>>>> bb673fcb744ff37f65ea4648580b1bbac7735113
   const uploadItem = () => {
     let flag = true;
     Object.values(inputsValues).forEach((text) => {
@@ -45,7 +37,6 @@ function AddItem() {
       uploadBytes(imageRef, imageUpload)
         .then(() => {
           getDownloadURL(imageRef)
-<<<<<<< HEAD
             .then((url) => {
               const item = { ...inputsValues, imageURL: url };
               axios
@@ -59,16 +50,6 @@ function AddItem() {
                   });
                   setImageUpload(null);
                   toast.success("Item saved successfully");
-=======
-            .then(url => {
-              const item = { ...inputsValues, imageURL: url }
-              axios.post(`http://localhost:4000/item`, item)
-                .then(data => {
-                  console.log(data)
-                  setInputsValues({ title: ``, description: ``, category: `` })
-                  alert(`Item saved successfully`)
-
->>>>>>> bb673fcb744ff37f65ea4648580b1bbac7735113
                 })
                 .catch((error) =>
                   toast.error(
@@ -121,14 +102,9 @@ function AddItem() {
             className={`input-add-item`}
             value={inputsValues[`category`]}
           >
-<<<<<<< HEAD
+            <option value={-1}>{`Select Item`}</option>
             {catagories.map((c, index) => (
               <option key={index} value={c}>
-=======
-            <option value= {-1}>{`Select Item`}</option>
-            {catagories.map((c,index) => (
-             <option key={index} value={c}>
->>>>>>> bb673fcb744ff37f65ea4648580b1bbac7735113
                 {c}
               </option>
             ))}
@@ -177,4 +153,3 @@ function AddItem() {
 }
 
 export default AddItem;
-
